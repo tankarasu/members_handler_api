98f8194312e75fed85bf94609b29d5b257de3561
Tan KARASU <karasutan@gmail.com>
2020-07-29 08:32:32 +0200

    MAJ des documents MongoDB et gestion etendu

--- src/model/member.js
+++ src/model/member.js
@@ -3,9 +3,18 @@ let Schema = Mongoose.Schema;
 
 let memberSchema = new Schema({
   id: Number,
-  name: String,
+  name: String, 
+  avatar: String, //à faire
+  lastName: String, //à faire
+  bornAt: String, //à faire
+  adress: String, //à faire
+  zip: Number, //à faire
+  city: String, //à faire
+  tel1: Number, //à faire
+  tel2: Number, //à faire
+  eMail: String, //à faire
 });
 
 let member = Mongoose.model("member", memberSchema);
 
-export {member}
+export { member };
--- src/routes/member.js
+++ src/routes/member.js
@@ -2,9 +2,9 @@
 import express from "express";
 import { member } from "../model/member.js";
 import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
 
 let router = express.Router();
-const db = mongoose.connection;
 
 let checkId = (id) => {
   if (!isNaN(id)) return true;
@@ -13,35 +13,53 @@ let checkId = (id) => {
 
 router
   .route("/users")
-  .get((req, res, next) => {
-    member.find({}, (err, result) => {
-      if (err) throw err;
-      res.json(result);
-      next();
-    });
+  .get((req, res) => {
+    member
+      .find({})
+      .exec()
+      .then((name) => res.json(name))
+      .catch((err) => res.send(err));
   })
-  .post(async (req, res, next) => {
-    let user = new member({ id: 1, name: req.body.name });
+  .post(async (req, res) => {
+    let user = new member({
+      id: 1,
+      name: req.body.name,
+      lastName: req.body.lastName,
+      avatar: req.body.avatar,
+      bornAt: req.body.bornAt,
+      adress: req.body.adress,
+      zip: req.body.zip,
+      city: req.body.city,
+      tel1: req.body.tel1,
+      tel2: req.body.tel2,
+      eMail: req.body.eMail,
+    });
+    let data = [];
 
-    await member.create(user, (err, result) => {
-      if (err) throw err;
-      res.send(result.name + " added to DB");
-      next();
-    });
-    await member.find({}, (err, result) => {
-      if (err) throw err;
-      result.forEach((element) => {
-        member.findByIdAndUpdate(
-          element._id,
-          { id: result.indexOf(element) },
-          { useFindAndModify: false },
-          (err) => {
-            if (err) throw err;
-          }
-        );
-      });
-      next();
-    });
+    await member
+      .create(user)
+      .then((newUser) => {})
+      .catch((err) => res.send(err));
+    //on met à jour l'id visible sur l'UI
+    await member
+      .find({})
+      .then((userList) => {
+        data = userList;
+        data.forEach((element) => {
+          console.log(element._id);
+          member.findOneAndUpdate(
+            { _id: element._id },
+            { id: data.indexOf(element) },
+            { new: true },
+            (err, result) => {
+              if (err) throw err;
+              console.log(result);
+            }
+          );
+        });
+      })
+      .then("ok")
+      .catch((err) => console.log(err));
   });
 
 router
@@ -51,11 +69,11 @@ router
       res.send("Not a valid ID");
       return;
     }
-    member.findOne({ id: req.params.id }, (err, result) => {
-      if (err) throw err;
-      res.json(result);
-      next();
-    });
+    member
+      .findOne({ id: req.params.id })
+      .exec()
+      .then((name) => res.json(name))
+      .catch((err) => res.send(err));
   })
   .put(async (req, res, next) => {
     if (!checkId(req.params.id)) {
@@ -67,11 +85,8 @@ router
       { name: req.body.name },
       (err, result) => {
         if (err) throw err;
-        member.findOne({ id: req.params.id }, (err, result) => {
-          if (err) throw err;
-          res.json(result);
-          next();
-        });
+        member.findOne({ id: req.params.id }, (err, result) => {});
+        next();
       }
     );
   })
@@ -100,5 +115,39 @@ router
       next();
     });
   });
+
+router.route("/borndate/:id").put(async (req, res, next) => {
+  if (!checkId(req.params.id)) {
+    res.send("Not a valid ID");
+    return;
+  }
+  console.log(req.body.born);
+  await member.updateOne(
+    { id: req.params.id },
+    { bornAt: req.body.born },
+    (err, result) => {
+      if (err) throw err;
+      res.json(result);
+      next();
+    }
+  );
+});
+
+router.route("/lastname/:id").put();
+router.route("/avatar/:id").put();
+router.route("/adress/:id").put();
+router.route("/zip/:id").put();
+router.route("/city/:id").put();
+router.route("/tel1/:id").put();
+router.route("/tel2/:id").put();
+router.route("/mail/:id").put();
+
+router.route("/getname").get((req, res) => {
+  member
+    .find({})
+    .exec()
+    .then((name) => res.json(name))
+    .catch((err) => res.send(err));
+});
 
 export { router };
